d4444910641dedd8d276c08e3546a8056a1b16ec
mark-forster <jakeriley082@gmail.com>
2025-09-18 12:35:52 +0630

    modify config file for redirect

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '_redirects',
-          dest: ''
-        }
-      ]
-    })
-  ],
-  build: {
-    sourcemap: false,
-     minify: 'esbuild',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          ui: ['@chakra-ui/react', '@emotion/react', '@emotion/styled'],
-          utils: ['axios', 'socket.io-client', 'recoil']
-        }
-      }
-    }
-  },
+  plugins: [react()],
   server: {
     port: 3000,
+    // Get rid of the CORS error
     proxy: {
       "/api/v1": {
-        target: process.env.VITE_API_URL || "http://localhost:8000",
+        // Use the environment variable to set the correct target URL
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,
       },
     },
   },
